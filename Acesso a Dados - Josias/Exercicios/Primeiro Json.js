da3102a2440d0db0dae2ce7c60febee2c7d66ad0
edmarcoslins <elmf_lins@hotmail.com>
2016-08-17 17:15:45 -0300

    Primeiro Json

---
+++ Acesso a Dados - Josias/Exercicios/Primeiro Json.js
@@ -0,0 +1,33 @@
+{  
+   "produtos":{  
+   
+      "pendrive":[  
+         {  
+            "foto":"http://imagem.com/pendrive.jpg",
+            "nome":"Pen Drive",
+            "capacidade":"8gb",
+            "marca":"SanDisk",
+            "preco":15.99,
+            "cor":[  
+               "verde",
+               "preto",
+               "azul"
+            ]
+         },
+		 
+         {  
+            "foto":"http://imagem.com/pendrive.jpg",
+            "nome":"Pen Drive",
+            "capacidade":"8gb",
+            "marca":"SanDisk",
+            "preco":15.99,
+            "cor":[  
+               "verde",
+               "preto",
+               "azul"
+            ]
+         }
+      ]
+	  
+   }
+}
